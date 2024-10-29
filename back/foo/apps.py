63aa29dbba6c17238ddc25c93775f806441ddbec
jossszzz <josephgvalfa@gmail.com>
2024-10-28 22:13:21 -0500

    Add comment

--- back/foo/apps.py
+++ back/foo/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
+# Add comment
 class FooConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'foo'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "foo"
